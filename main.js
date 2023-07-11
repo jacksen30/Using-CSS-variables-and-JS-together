16e57b6bb5ec03ed2115682c192cb2708d7855fb
Jacksen Nillson <nillsonjacksen@gmail.com>
2023-07-11 12:37:59 +0800

    update default variable values and slider positions to match, comment out 'change' event listener

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ function handleUpdate() {
     const suffix = this.dataset.sizing || '';
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
     }
-
-inputs.forEach(input => input.addEventListener('change', handleUpdate));
     
 /* 'mousemove' wouldn't work for dynamically updating change on input so changed to 'input' */
 inputs.forEach(input => input.addEventListener('input', handleUpdate));
+
+/* inputs.forEach(input => input.addEventListener('change', handleUpdate)); */
